e599d230d3077af8ecea3b1fc9e4ebc127a0057b
labeveryday <29664932+labeveryday@users.noreply.github.com>
2021-10-14 11:01:59 -0500

    Added tests

--- src/tests/test_cars.py
+++ src/tests/test_cars.py
@@ -10,6 +10,16 @@ class TestMain(unittest.TestCase):
         """Send an API request to test connectivity."""
         response = cars.get_porsche()
         self.assertTrue(response)
+    
+    def test_build_search(self):
+        """Test if path"""
+        search = cars.build_search("porsche")
+        self.assertEqual(search, "porsche")
+    
+    def test_build_search_else(self):
+        """Test verify else path"""
+        search = cars.build_search("porsche 1990 944")
+        self.assertEqual(search, "porsche%201990%20944")
 
 
 if __name__ == "__main__":
